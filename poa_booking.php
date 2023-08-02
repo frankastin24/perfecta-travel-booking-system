013d0cf15d47e3e3bf9ea1d2267f31ad08a59f67
Frank Astin <frankastin2023@gmail.com>
2023-08-02 02:46:36 +0100

    finished calendar

--- poa_booking.php
+++ poa_booking.php
@@ -37,32 +37,130 @@ class POA_Booking
 
         ksort($this->months_containing_startdates);
 
-        $this->generate_script();
-        $this->generate_styles();
 
-        echo  $this->generate_calendar_html();
 
-        exit;
+
+        $this->calender_data = $this->generate_months_data();
+        add_shortcode('poa_booking', array($this, 'shortcode'));
     }
 
-    public function generate_calendar_html()
+    public function generate_months_data()
     {
-        $calendar_html = '';
+        $months = array();
         foreach ($this->months_containing_startdates as $month => $dates) {
-            $calendar_html .= $this->generate_month_html($month, $dates);
+            $months[]  = $this->generate_month_data($month, $dates);
         }
-        return $calendar_html;
+        return $months;
     }
-
-    public function generate_month_html($month, $dates)
+    public function shortcode()
     {
+
+
+        $data = json_encode($this->calender_data);
+
+        $this->generate_styles();
+        return <<<END
+            <div class='calendar'>
+            <header><select id="month-select"></select></header>
+            <div class="day-names"></div>
+            <div class="days"></div>
+            </div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+            <script>
+           
+            const calendar_data = $data;
+
+            let selectOptionsHTML =  '';
+            const populateSelect = (month,index) => {
+                
+                let optionsHTML = '';
+
+                calendar_data.forEach((month) => {
+                    optionsHTML += '<option value="'+month.name+'">'+month.name+'</option>';
+                });
+
+                $('#month-select').html(optionsHTML);
+              
+         
+            
+            }
+
+            
+            
+
+            const populateCalendar = (month,index) => {
+                
+                let daysHTML = '';
+
+                month.days.forEach((day) => {
+                    daysHTML += '<div class="day '+ day.class + '">'+day.number+'</div>';
+                });
+
+                $('.days').html(daysHTML);
+              
+             
+
+            
+            }
+
+
+
+
+            $(() => {
+                populateSelect();
+                populateCalendar(calendar_data[0],0);
+
+                $('#month-select').on('change', (e) => {
+                    const index = $(e.currentTarget).find(":selected").index();
+                    console.log(index);
+                    populateCalendar(calendar_data[index],index);
+                });
+                $('body').on('mouseover','.active', (e) => {
+                    const index = $(e.currentTarget).index();
+                    $(e.currentTarget).addClass('hover')
+                    const length = index + 6;
+                    for (let x = index; x < length; x ++) {
+                        $('.day').eq(x).addClass('hover');
+                    }
+                })
+                $('body').on('mouseout','.active', (e) => {
+                    $('.hover').removeClass('hover')
+                })
+                $('body').on('click','.active', (e) => {
+                    const index = $(e.currentTarget).index();
+                    const length = index + 6;
+                    if( $(e.currentTarget).hasClass('selected') ) {
+                        $(e.currentTarget).removeClass('selected')
+                        for (let x = index; x < length; x ++) {
+                            $('.day').eq(x).removeClass('selected');
+                        }
+                    } else {
+                    $('.selected').removeClass('selected')
+
+                    $(e.currentTarget).addClass('selected')
+                  
+                    for (let x = index; x < length; x ++) {
+                        $('.day').eq(x).addClass('selected');
+                    }
+                }
+                })
+                $('body').on('click','.selected', (e) => {
+                    $('.selected').removeClass('selected');
+                })
+            });
+
+            </script>";
+            END;
+    }
+    public function generate_month_data($month, $dates)
+    {
+
+        $data = [];
+
 
         $monthDateObj  = DateTime::createFromFormat('Y-m-d', $dates[0]);
 
-        $monthName = $monthDateObj->format('F');
-
-
-        $month_html = '<div class="month"><header>' . $monthName . '</header><div class="day-names"><div>M</div><div>T</div><div>W</div><div>T</div><div>F</div><div>S</div><div>S</div></div><div class="days">';
+        $data['name'] = $monthDateObj->format('F');
 
 
         $prev_month_num_days = cal_days_in_month(CAL_GREGORIAN, intval($month) - 1, 2023);
@@ -80,8 +178,10 @@ class POA_Booking
         $lastDayPrevMonth = intval($lastDayPrevMonthObj->format('d'));
         $lastDayDate =  intval($lastDayDateObj->format('w'));
 
+        $data['days'] = [];
+
         for ($i = 0; $i < $number_of_additional_days; $i++) {
-            $month_html .= '<div class="day grey">' . $lastDayPrevMonth - ($number_of_additional_days - $i) . '</div>';
+            $data['days'][] = array('class' => 'grey', 'number' =>  $lastDayPrevMonth - ($number_of_additional_days - $i));
         }
 
 
@@ -96,15 +196,15 @@ class POA_Booking
                 $active = true;
             }
 
-            $month_html .= '<div class="day ' . ($active ? 'active' : 'false') . '">' . strval($i + 1) . '</div>';
+            $data['days'][] = array('class' => ($active ? 'active' : 'false'), 'number' => strval($i + 1));
         }
 
         for ($i = 0; $i < (7 - $lastDayDate); $i++) {
-            $month_html .= '<div class="day grey">' .  ($i + 1) . '</div>';
+            $data['days'][] = array('class' =>  'grey', 'number' => strval($i + 1));
         }
 
-        $month_html .= '</div></div>';
-        return $month_html;
+
+        return $data;
     }
 
     public function generate_styles()
@@ -149,6 +249,16 @@ class POA_Booking
 
             .active {
                 background: #d3ffd0;
+                cursor: pointer;
+            }
+
+            .hover {
+                background: blue;
+            }
+
+            .selected {
+                background: red;
+                cursor: pointer;
             }
         </style>
 <?php
